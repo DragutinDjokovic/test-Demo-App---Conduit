f060f313e9bd007eff0e035057752842de24c0f8
Dragutin <dragutindjokovic@gmail.com>
2020-09-24 18:15:06 +0200

    First commit

--- cypress/integration/testScenarios/1.LoginLogout.spec.js
+++ cypress/integration/testScenarios/1.LoginLogout.spec.js
@@ -1,9 +1,13 @@
-describe('Login', () => {
+describe('Login and Logout', () => {
     it('Sign in', () =>{
         cy.visit('https://react-redux.realworld.io/#/login')
         cy.get('input[type="email"]').type('ddjwint@gmail.com')
         cy.get('input[type="password"]').type('Haveanaccount?')
         cy.get('.btn').contains('Sign in').should('be.visible').click()
         cy.get('.navbar-brand').click()
+        cy.get(':nth-child(4) > .nav-link').click()
+        cy.get('.btn').click()
+        cy.get('.btn-outline-danger').click()
+        cy.get(':nth-child(3) > .nav-link').should('be.visible')
     })
 })
